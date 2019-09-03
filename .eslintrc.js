1beb1f590041538369c7a076ed569857fa23e8b4
Marcelo Block Teixeira <marcelochb@gmail.com>
2019-09-03 15:12:18 -0300

    corrigindo eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
   plugins: ['react', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
-    'react/jsx-filename-extension': ['warn', { extension: ['.jsx', '.js'] }],
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
     'import/prefer-default-export': 'off',
   },
 };
